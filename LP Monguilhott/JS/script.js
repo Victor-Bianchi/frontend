063052b6e51839c9c4a01b644701ef09013e258d
Victor Nunes Bianchi <72978087+Victor-Bianchi@users.noreply.github.com>
2021-08-11 18:14:50 -0300

    Atualização 11/08/2021
    Atualização do projeto Monguilhott, isso inclui:
    - Novas Sessões
    - Novas Imagens
    - Maior conteúdo no CSS
    - Formatação completa para Design Responsivo (até a última sessão atual)

--- LP Monguilhott/JS/script.js
+++ LP Monguilhott/JS/script.js
@@ -1,6 +1,6 @@
-$(function(){
-    $('nav a').hover(function () { 
-       $('nav li').removeClass('selected')
-       $('nav li').addClass('selected')
-    });
+$(function () {
+    $('nav a').hover(function () {
+        $('nav li').removeClass('selected');
+        $(this).parent().addClass('selected') // no que estou com o mouse
+    })
 })
