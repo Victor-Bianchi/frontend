ba73f3d8fabc8b655c7927673f9e097ab8ebf891
Victor Nunes Bianchi <72978087+Victor-Bianchi@users.noreply.github.com>
2021-08-12 17:16:10 -0300

    Atualização 12/08/2021
    Adição dos seguintes itens:
    - Término do Front-end do site
    - Alteração e adição de propriedades CSS
    - Novas sessões
    - Pequena alteração no arquivo JS

--- LP Monguilhott/JS/script.js
+++ LP Monguilhott/JS/script.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('nav a').hover(function () {
+    $('nav.desktop a').hover(function () {
         $('nav li').removeClass('selected');
         $(this).parent().addClass('selected') // no que estou com o mouse
     })
